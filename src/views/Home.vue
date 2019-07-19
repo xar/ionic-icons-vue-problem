313e5762f6a73715dfcf20489157d31b1d7bbb95
samovrablik <samo@vrablik.eu>
2019-07-19 10:33:21 +0200

    Init

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Hello World</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h1>Welcome To @ionic/vue</h1>
+      <ion-icon name="fastforward"></ion-icon>
+      <img alt="Vue logo" src="../assets/logo.png">
+    </ion-content>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
